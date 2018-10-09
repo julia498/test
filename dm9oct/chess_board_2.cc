cc27a30eae8de0462ad0c088fc1d9ec5c1f6beca
julia498 <julia.lloveras@estudiant.upc.edu>
2018-10-09 12:26:59 +0200

    info final classe dm9oct

--- dm9oct/chess_board_2.cc
+++ dm9oct/chess_board_2.cc
@@ -9,7 +9,7 @@ int main(){
         for (int j = 0; j < c; ++j){
             char n;
             cin >> n;
-            t += n - '0';
+            if ((i+j)%2 == 0) t += n - '0';
         }
         
     }
