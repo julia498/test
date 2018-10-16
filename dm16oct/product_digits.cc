ea09ea6629f06a65fdf4e33d812928ebbcdc6eaf
julia498 <julia.lloveras@estudiant.upc.edu>
2018-10-16 12:20:03 +0200

    infooo (em fa pal fer mes coses... son i 19... he de solucionar el problema prod_digits de la carpeta dm16oct

--- dm16oct/product_digits.cc
+++ dm16oct/product_digits.cc
@@ -3,6 +3,33 @@ using namespace std;
 
 
 int main () {
+    int n;
+    while (cin >> n){
+        int i = n, t = 1;
+        while (n > 10){
+            cout << "The product of the digits of " << i << " is ";
+            while (i > 0){
+                t *= i%10;
+                i /= 10;
+            }
+            cout << t << "." << endl;
+            n /= 10;
+            i = t;
+            t = 1;
+       
+    }
+    
+    
+    
+    
+    
+    
+    
+    #include <iostream>
+using namespace std;
+
+
+int main () {
     int n, l = 1;
     while (cin >> n){
         if (l != 1) cout << "----------" << endl;
@@ -20,6 +47,16 @@ int main () {
             t = 1;
         }
     }
+    
+    
+}
+
+    
+//        for (int i = n; i < 10; i/10){
+  //          int p = 1;
+    //        p *= i%10;
+      //      cout << "The product of the digits of " << n << " is " << p << "." << endl;
+        }
     
     
 }
