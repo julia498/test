1c62bbcd6eb0c78ab32b5040417dab40998b52bd
julia498 <julia.lloveras@estudiant,upc.edu>
2018-10-25 13:54:08 +0200

    final info ara si

---
+++ dj25oct/looking_a.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    char n;
+    bool found = false;
+    while (cin >> n and not found){
+        if (n == '.') cout << "no" << endl;
+        if (n == 'a'){
+            found = true;
+            cout << "yes" << endl;
+    }
+    
+}
