35187b2bb44ebc25dcbd0a581bc05090afed94f0
julia498 <julia.lloveras@estudiant.upc.edu>
2018-11-15 13:56:50 +0100

    info 15nov final classe

--- 15nov/counting_frequences.cc
+++ 15nov/counting_frequences.cc
@@ -7,12 +7,6 @@ using namespace std;
 int main() {
     int n, a;
     cin >> n;
-    vector<int> v;
-    for (int i=0; i<n; ++i) {
-        cin >> a;
-        v.push_back(a);
-    }
-    for (int i=0; i<n; ++i) {
-        
-    }
+    vector<int> v(1001);
+
 }
