9234e6168f25343909300c83880eab0d545d66ef
julia498 <julia.lloveras@estudiant.upc.edu>
2018-12-04 12:27:52 +0100

    final info

--- 4nov/array_intersection.cc
+++ 4nov/array_intersection.cc
@@ -3,5 +3,16 @@ using namespace std;
 #include <vector>
 
 vector<double> intersection(const vector<double>& v1, const vector<double>& v2) {
-    
+    vector<double> v;
+    int i=0, j=0;
+    while (i < (int)v1.size() and j < (int)v2.size()) {
+        if (v1[i] < v2[j]) ++i;
+        else if (v1[i] > v2[j]) ++j;
+        else {
+            if (v1[i] != v[(int)v.size()-1]) v.push_back(v1[i]);
+            ++i;
+            ++j;
+        }
+    }
+    return v;
 }
