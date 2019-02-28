3a07a8b22cf2df7d1ecd42f0ea337b792c0d36be
julia498 <julia.lloveras@estudiant.upc.edu>
2019-02-28 13:30:10 +0100

    dlkhnjskdn

---
+++ numerica/28feb/main.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main(int argc, char *argV[]) {
+    cout << argV[1] << endl;
+    
+    ifstream fitxerDades;
+    fitxerDades.open(argV[1], ifstream::in);
+    if(fitxerDades.fail()) {  //si no s'ha obert el fitxer
+        cout << "kahffsuh" << endl;
+    }
+    
+    return 0;
+}
